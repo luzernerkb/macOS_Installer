2427982318ddf521c0454cbe7de4f533e3f42d27
Tom Enz [LU14995] <tom.enz@lukb.ch>
2021-02-03 18:25:49 +0100

    Removed unused output

--- setupUser.sh
+++ setupUser.sh
@@ -20,7 +20,6 @@ read -p "Please enter your firstname:  " USER_FIRSTNAME
 read -p "Please enter your lastname:  " USER_LASTNAME
 read -p "Please enter your LUKB email:  " USER_EMAIL
 read -p "Please enter your LUKB ad account:  " USER_ACCOUNT
-echo $USER_ACCOUNT | tr [:lower:] [:upper:]
 todo "Hello ${USER_FIRSTNAME} ${USER_LASTNAME}, with email: ${USER_EMAIL} and account: ${USER_ACCOUNT}"
 read -p "Please check, is everything correct? Press any key to continue... (CTRL+C to cancel) " -n1 -s
 echo -e "\n"
